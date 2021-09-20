19a4304cc76d62df8cde14d1c7310d5c0845cbb5
Ken Nguyen <kenn.tnguyen@gmail.com>
2021-09-19 22:27:59 -0400

    [TS-15] Initialize cypress test and config

---
+++ src/components/HelloWorld.spec.js
@@ -0,0 +1,9 @@
+import { mount } from '@cypress/vue';
+import HelloWorld from './HelloWorld.vue';
+
+it('Test Hello World component', () => {
+	// with JSX
+	mount(() => <HelloWorld msg="Welcome to Your Vue.js App" />);
+
+	cy.get('h1').should('contain', 'Welcome to Your Vue.js App');
+});
