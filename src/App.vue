464a1a4bb253c5ba8016e03b7021e91288db0526
rileykc98 <rileykilmerchoi@hotmail.com>
2021-10-05 12:34:36 -0400

    [TS-95] Add routes for existing pages

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <template>
-  <!-- <Brokerage /> --> <!--uncomment to see brokerage page --> 
-  <!-- <Login /> --> <!--uncomment to see login page --> 
-  <SignUp />
+  <router-view />
 </template>
 
 <script>
-//import Brokerage from "./pages/BrokerageSignUp/App.vue" //uncomment to see brokerage page
-//import Login from "./pages/Login/App.vue" //uncomment to see login page
-import SignUp from "./pages/SignUp/App.vue"
-
 export default {
   name: "App",
-  components: {
-    //Brokerage, //uncomment to see brokerage page
-    //Login //uncomment to see login page
-    SignUp
-  },
+  components: {},
 };
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import SignUp from '../pages/SignUp/App.vue'
+import Login from '../pages/Login/App.vue'
+import BrokerageSignUp from '../pages/BrokerageSignUp/App.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Login
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/SignUp',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/Login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path:'/BrokerageSignUp',
+    name: 'BrokerageSignUp',
+    component: BrokerageSignUp
   }
 ]
 
