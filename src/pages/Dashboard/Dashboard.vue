914541ea55fe122ea03d752bbe7017ff4543bc89
bowe99 <kevinbowe1999@gmail.com>
2021-10-14 18:51:45 -0400

    [TS-113][TS-114][TS-115] Implement Portfolio Analysis Components
    Co-authored-by: Alya Naseer <AlyaNaseer@users.noreply.github.com>

--- src/pages/Dashboard/Dashboard.vue
+++ src/pages/Dashboard/Dashboard.vue
@@ -1,14 +1,26 @@
 <template>
-  <Profile />
+  <!-- <Profile/> -->
+  <v-row>
+    <v-col>
+      <PortfolioAnalyticsTemplate title="Portfolio Value" value="$24,000" :percentChange="-20" icon="mdi-domain"/>
+    </v-col>
+    <v-col>
+      <PortfolioAnalyticsTemplate title="Daily Change" value="+2.12%" :percentChange="16" icon="mdi-calendar-today"/> </v-col
+    ><v-col>
+      <PortfolioAnalyticsTemplate title="Goal Progress" value="75%" progress="75" color="blue" icon="mdi-progress-clock"/>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-import Profile from "../../components/Dashboard/Profile.vue"
+// import Profile from "../../components/Dashboard/Profile.vue"
+import PortfolioAnalyticsTemplate from "../../components/Portfolio/PortfolioAnalyticsTemplate.vue";
 
 export default {
   name: "Dashboard",
   components: {
-    Profile,
+    // Profile,
+    PortfolioAnalyticsTemplate,
   },
 };
 </script>
