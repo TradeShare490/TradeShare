82cd28263388d7f1a128d5f8a01af17beed4939c
GadoliniumPotassium <gdkh514@gmail.com>
2021-10-14 23:40:18 -0400

    [TS-122]: Fixed error with v-img tag

---
+++ src/components/Notification/Notifications.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-alert 
+    prominent
+    dismissible
+    v-model="alert"
+    color="cyan"
+    border="left"
+    elevation="2"
+    colored-border
+    transition="fade-transition"
+    icon="../assets/logo.png"> <!-- Eventually here we're going to have to find a way to make it so that the icon is the users profile pic -->
+        <v-row align='center'>
+            <v-col>
+                <v-img 
+                 src="{{profilePic}}"
+                 max-height="100"
+                 max-width="100">  
+                </v-img>    
+            </v-col>
+            
+            <v-col>
+                {{name}} has now followed you
+            </v-col>
+            
+            <v-col>
+                <v-btn color="blue" @click="follow()">Follow Back</v-btn>
+            </v-col>
+        <v-row>
+
+    </v-alert>
+</template>
