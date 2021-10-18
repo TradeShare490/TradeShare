38082f597750a89463d4e110b57cc584e0ee32a0
GadoliniumPotassium <gdkh514@gmail.com>
2021-10-18 15:48:07 -0400

    [TS-122]: Notification Bar moved to proper page and fits specifications finally.

--- src/components/Notification/Notifications.vue
+++ src/components/Notification/Notifications.vue
@@ -1,42 +1,63 @@
 <template>
-    <v-alert 
-    type="info"
-    dismissible
-    color="primary"
-    border="left"
-    elevation="2"
-    colored-border
-    transition="fade-transition"
-    icon=false>
-        <v-row align='center'>
-            <v-col
-             align-self="center"
-             class="px-0 py-5"
-             cols="2"
-             sm="2"
-             md="1"
-             lg="1"
-             xl="1"
+  <v-card elevation="0" class="px-10">
+    <v-row no-gutters>
+      <v-col
+        align-self="center"
+        class="px-0 py-5"
+        cols="2"
+        sm="2"
+        md="1"
+        lg="1"
+        xl="1"
       >
-              <v-avatar class="profile" size="50">
-                <v-img 
-                 :src="image"
-                 max-height="75"
-                 max-width="75">  
-                </v-img> 
-              </v-avatar>
-            </v-col>
-            
-            <v-col>
-                {{this.message}}
-            </v-col>
-            
-            <v-col sm="3" md="2" lg="2" xl="2" align-self="center">
-                <v-btn width="250" height="45" color="primary" @click="follow()">Follow Back</v-btn>
-            </v-col>
-        </v-row>
-
-    </v-alert>
+        <v-icon large color="blue">
+          mdi-exclamation
+        </v-icon>
+        <v-avatar class="profile" size="50">
+          <v-img :src="image"></v-img>
+        </v-avatar>
+      </v-col>
+      <v-col
+        align-self="center"
+        cols="6"
+        sm="3"
+        md="3"
+        lg="3"
+        xl="3"
+        class="text-left mx-0 px-0 mt-3 py-0"
+      >
+       {{this.message}}
+      </v-col>
+      
+      <v-spacer></v-spacer>
+      <v-col cols="4" sm="3" md="2" lg="2" xl="2" align-self="center">
+        <v-btn
+          block
+          v-bind="size"
+          elevation="0"
+          outlined
+          color="primary"
+          v-if="following == true"
+          @click="unfollow"
+          class="my-3 caption"
+        >
+          Following
+        </v-btn>
+        <v-btn
+          block
+          v-bind="size"
+          elevation="0"
+          color="primary"
+          v-if="following == false"
+          @click="follow"
+          class="my-3 caption"
+        >
+          Follow
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-divider></v-divider>
+  </v-card>
 </template>
 
 <script>
@@ -63,6 +84,18 @@ export default {
         this.labelText = "untagged";
         this.labelColor = "untagged";
         console.log("following...");
+      },
+    },
+      computed: {
+      size() {
+        const size = {
+          xs: "x-small",
+          sm: "small",
+          md: "small",
+          lg: "small",
+          xl: "small",
+        }[this.$vuetify.breakpoint.name];
+        return size ? { [size]: true } : {};
       },
     },
   } 
--- src/pages/Dashboard/Dashboard.vue
+++ src/pages/Dashboard/Dashboard.vue
@@ -9,32 +9,20 @@
     ><v-col>
       <PortfolioAnalyticsTemplate title="Goal Progress" value="75%" progress="75" color="blue" icon="mdi-progress-clock"/>
     </v-col>
-    <v-col>
-      <FollowNotif
-      :currentlyFollowing="false"
-      currentLabelColor="friend"
-      currentLabelText="friend"
-      image="https://randomuser.me/api/portraits/men/52.jpg"
-      message="John Molson has followed you :)"
-      >
-
-      </FollowNotif>
-    </v-col>
   </v-row>
+
 </template>
 
 <script>
 // import Profile from "../../components/Dashboard/Profile.vue"
 // Adding follow function to script
 import PortfolioAnalyticsTemplate from "../../components/Portfolio/PortfolioAnalyticsTemplate.vue";
-import FollowNotif from "../../components/Notification/Notifications.vue";
 
 export default {
   name: "Dashboard",
   components: {
     // Profile,
     PortfolioAnalyticsTemplate,
-    FollowNotif
   },
 };
 </script>
