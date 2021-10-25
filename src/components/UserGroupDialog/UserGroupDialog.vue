663a6448a2eff65aa568e3c3ce0460d749b45d95
Ming424 <tianming.chen0424@gmail.com>
2021-10-25 13:41:18 -0400

    User Group Dialog Frontend finish

--- src/components/UserGroupDialog/UserGroupDialog.vue
+++ src/components/UserGroupDialog/UserGroupDialog.vue
@@ -1,120 +1,204 @@
 <template>
+<div>
   <v-row justify="center">
+    <v-btn color="primary" class="ma-2" dark @click="dialog = true">
+      Open Dialog 1
+    </v-btn>
+    
+    <!-- DIALOG #1 -->
     <v-dialog v-model="dialog" scrollable max-width="300px">
-
-      <!-- btn to trigger dialog -->
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn color="primary" dark v-bind="attrs" v-on="on">
-          > User Group Dialog
-        </v-btn>
-      </template>
-      
-      <!-- title -->
       <v-card class="px-1 pb-1">
-        <v-card-title v-on:click="say('on click title')">
+
+        <v-card-title @click="renderList()">
           <v-icon>mdi-tag</v-icon>&nbsp;Label
         </v-card-title>
-
         <v-divider></v-divider>
         
-        <v-card-text style="height: 200px;">
-          <v-radio-group v-model="taskGroup" column >
-
+        <v-card-text style="height: 210px;" class="pb-0">
+          <v-radio-group v-model="index" column>
             <!-- loop render group list -->
-            <div v-for="grp in groups" :key="grp.name">
-              <v-radio class="px-auto pb-1" > 
+            <div v-for="(group, index) in groups" :key="group.index">
+              <v-radio class="px-auto pb-1"> 
                 <template v-slot:label >
-                  <div v-bind:style="{ color: grp.color }"> {{grp.name}} </div>
+                  <div v-bind:style="{ color: group.color }"> {{group.name}} </div>
                   &nbsp;
                   <!-- btn for edit pane -->
-                  <v-icon v-on:click="say('on click edit icon') " @click="dialog3 = !dialog3">mdi-pencil-box-outline</v-icon>
+                  <v-icon @click="openEdit(index)">mdi-pencil-box-outline</v-icon>
                 </template>
               </v-radio>
             </div>
-
           </v-radio-group>
         </v-card-text>
-        <h6>Selected Radios: {{taskGroup}}</h6>
 
-        
-
+        <!-- <h6>Selected Radios: {{index}} {{groups[index].name}}</h6> -->
         <v-btn elevation="0"  block v-on:click="addGroup()" class="my-2">
           <v-icon>mdi-account-plus</v-icon>
         </v-btn>
-        <v-btn elevation="0" color="success" block v-on:click="submit()">
+        <v-btn elevation="0" color="success" block v-on:click="submit()" @click="dialog = false">
           Save
         </v-btn>
 
       </v-card>
     </v-dialog>
 
-
-    <v-dialog v-model="dialog3" max-width="350px">
+    <!-- DIALOG #2 GROUP EDIT -->
+    <v-dialog v-model="dialog2" max-width="350px">
       <v-card>
         <v-card-title class="d-flex justify-center">
-          <v-color-picker  flat  dot-size="25" hide-inputs swatches-max-height="200" hide-mode-switch :show-swatches="false"></v-color-picker>
-          <v-text-field value="" placeholder="Group Name"></v-text-field>
+          <v-color-picker flat v-model="color" hide-inputs swatches-max-height="200" hide-mode-switch :show-swatches="false"></v-color-picker>
+          <v-text-field v-model="groupNameInput" :rules="[rules.required, rulesGroupName.max]" maxlength="10" placeholder="Group Name"></v-text-field>
         </v-card-title>
         <v-card-actions class="d-flex justify-center">
-          <v-btn color="success" style="width: 230px;" v-on:click="editGroup()" @click="dialog3 = false">
+          <v-btn color="success" style="width: 230px;" @click="editGroup(index)">
             <v-icon>mdi-check</v-icon>
           </v-btn>
-          <v-btn color="error"  outlined @click="dialog3 = false">
+          <v-btn color="error"  outlined  @click="dialog3= !dialog3">
             <v-icon>mdi-account-remove</v-icon>
           </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
 
+    <!-- DIALOG #2 GROUP DEL CONFIRMATION -->
+    <v-dialog v-model="dialog3" persistent max-width="370">
+      <v-card>
+        <v-card-title class="text-h5"> Delete Group [{{ groups[index].name}}] ? </v-card-title>
+        <v-card-text>All User belongs to the group will be set to default.</v-card-text>
+        <v-card-actions class="d-flex justify-center pt-0">
+          <v-btn color="secondary" outlined style="width: 130px;" @click="dialog3 = false">
+            <v-icon>mdi-cancel</v-icon>
+          </v-btn>
+          <v-btn color="error" style="width: 190px;" @click="delGroup(index)" class="mr-1">
+            <v-icon>mdi-account-remove</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <!-- DYNAMIC SNACKBAR (NOTIFICATION) -->
+    <v-snackbar v-model="snackbar" :snackbarTimeout="snackbarTimeout" :color="snackbarColor">
+      {{snackbarText}}
+      <template v-slot:action="{ attrs }">
+        <v-btn text v-bind="attrs" @click="snackbar = false">Close</v-btn>
+      </template>
+    </v-snackbar>
+
   </v-row>
+</div>
 </template>
 
 <script>
   export default {
-    data () {
-      return {
-        grp: "",
-        editedTask: null,
+    
+    data: () => ({
+        index: 0,
+        group: "",
+        groupNameInput: "",
         groups: [
           { name: "Family", color: "#3B7600"},
           { name: "School",  color: "#007652"},
           { name: "Friend",  color: "#430086"},
         ],
-        taskGroup: 1,
         dialog: false,
-        groupEdit: false,
-        radioGroup: 1,
-        radios: 'radio-1',
-      }
+        dialog2: false,
+        dialog3: false,
+        rules: {
+          required: (v) => !!v || "Required",
+        },
+        rulesGroupName: {
+          max: (v) => v.length <= 10 || "Max 10 characters",
+        },
+        // snack bar
+        snackbar: false,
+        snackbarText: "",
+        snackbarColor: "primary",
+        snackbarTimeout: 3000,
+        // for color picker
+        hex: '#FF0000',
+        type: 'hex',
+    }),
+
+    // color picker component
+    computed: {
+      color: {
+        get () { return this[this.type] },
+        set (v) { this[this.type] = v },
+      },
+      showColor () {
+        if (typeof this.color === 'string') return this.color
+        return JSON.stringify(Object.keys(this.color).reduce((color, key) => {
+          color[key] = Number(this.color[key].toFixed(2));   
+          return color
+        }, {}), null, 2)
+      },
     },
 
     methods: {
-      
-      say(msg) {
-        console.log(msg);
-        console.log()
+      renderList() {
+        console.log("renderList()");
+        for(let i = 0; i < this.groups.length; i++)
+            console.log("#" + i + " [" + this.groups[i].name + ", " + this.groups[i].color + "]");
+      },
+      openEdit(index) {
+        console.log("openEdit(" + index + ")");
+        this.hex = this.groups[index].color;
+        this.groupNameInput = this.groups[index].name;
+        this.dialog2 = true;
       },
       delGroup(index) {
-        this.groups.splice(index, 1);
+        console.log("delGroup(" + index + ")");
+        console.log(this.groups[index].name);
+        if(this.groups.length==1) {
+          this.snackbarText = "Keep at least one group";
+          this.snackbarColor = "error";
+        } else {
+          console.log("splice("+index+",1)");
+          this.groups.splice(index, 1); 
+          this.snackbarText = "User deleted";
+          this.snackbarColor = "success";
+        }
+        this.snackbar = true;
+        this.dialog3 = false;
+        setTimeout(() => {  this.dialog2 = false; }, 100);
+        this.renderList();
       },
-      editGroup() {
-        console.log("editGroup()");
-        console.log("taskGroup#:" + this.taskGroup + ":" + this.groups[this.taskGroup].name);
-        // this.grp = this.groups[index].name;
-        // this.editedTask = index;
+      editGroup(index) {
+        console.log("editGroup(" + index + ")");
+        const newName = this.groupNameInput.trim();
+        var item = this.groups[this.index];
+        var dupBoolean = 0;
+        for(let i = 0; i < this.groups.length; i++){
+          if(i!=this.index) {
+            var element = this.groups[i];
+            if(element.name.toUpperCase() == newName.toUpperCase()) {
+              dupBoolean = 1;
+              break;
+            }
+          }
+        }
+        if(newName !== "" && dupBoolean == 0){
+          item.name = newName;
+          this.groups[this.index].color = this.color;
+          this.dialog2 = false;
+          this.snackbarText = "User updated";
+          this.snackbarColor = "success";
+        } else {
+          this.snackbarText = "User update failed";
+          this.snackbarColor = "error";
+        }
+        this.snackbar = true;
       },
       addGroup() {
           this.groups.push({
-            name: "Group" + " " + (Math.random() + 1).toString(36).substring(7),
+            name: (Math.random() + 1).toString(36).substring(7),
             color: "#" + Math.floor(Math.random()*16777215).toString(16),
           });
-        this.grp = "";
+        this.group = "";
       },
       submit() {
         console.log("submit()");
-        console.log("taskGroup#:" + this.taskGroup + ":" + this.groups[this.taskGroup].name);
+        console.log("index#:" + this.index + ":" + this.groups[this.index].name);
       },
   },
-    }
-  
+}
 </script>
