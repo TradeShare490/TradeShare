0240a5efedba9b0b6cade7da826fbb470af71299
GadoliniumPotassium <gdkh514@gmail.com>
2021-10-25 16:04:13 -0400

    [TS-121]: attempting to resolve merge conflict

--- src/pages/Dashboard/Dashboard.vue
+++ src/pages/Dashboard/Dashboard.vue
@@ -1,14 +1,14 @@
 <template>
   <!-- <Profile/> -->
   <v-row>
-   <v-col>
+ <!--  <v-col>
       <PortfolioAnalyticsTemplate title="Portfolio Value" value="$24,000" :percentChange="-20" icon="mdi-domain"/>
     </v-col>
     <v-col>
       <PortfolioAnalyticsTemplate title="Daily Change" value="+2.12%" :percentChange="16" icon="mdi-calendar-today"/> </v-col
     ><v-col>
       <PortfolioAnalyticsTemplate title="Goal Progress" value="75%" progress="75" color="blue" icon="mdi-progress-clock"/>
-    </v-col> 
+    </v-col> -->
     <v-col>
       <BarChartContainer/> 
     </v-col>
@@ -19,14 +19,14 @@
 <script>
 // import Profile from "../../components/Dashboard/Profile.vue"
 // Adding follow function to script
-import PortfolioAnalyticsTemplate from "../../components/Portfolio/PortfolioAnalyticsTemplate.vue";
+//import PortfolioAnalyticsTemplate from "../../components/Portfolio/PortfolioAnalyticsTemplate.vue";
 import BarChartContainer from "../../components/ReturnGraphs/ReturnGraphs.vue"
 
 export default {
   name: "Dashboard",
   components: {
     // Profile,
-    PortfolioAnalyticsTemplate,
+    //PortfolioAnalyticsTemplate,
     BarChartContainer
   },
 };
