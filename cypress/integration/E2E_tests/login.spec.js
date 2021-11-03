d05660f8359d8e4ba8693467f0e9d6039a2387f4
Tung Leu <leudeptrai@gmail.com>
2021-11-02 23:04:29 -0400

    [TS-150] added wait time in test

--- cypress/integration/E2E_tests/login.spec.js
+++ cypress/integration/E2E_tests/login.spec.js
@@ -2,6 +2,7 @@ describe('Login Test', () => {
     it('Login', () => {
         cy.visit('http://localhost:8081/login')
         cy.login()
+        cy.wait(2000)
         cy.get("[data-cy=profile]").should('include.text', 'Jane Doe')
     })
 })
