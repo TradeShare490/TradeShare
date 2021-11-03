f6c840c4fb11eab966c7d499e0db49b0588c7c10
Tung Leu <leudeptrai@gmail.com>
2021-11-02 21:13:11 -0400

    [TS-150] Changed test file name

--- cypress/integration/E2E_tests/login.spec.js
+++ cypress/integration/E2E_tests/login.spec.js
@@ -1,5 +1,5 @@
 describe('Login Test', () => {
-    it.only('Login', () => {
+    it('Login', () => {
         cy.visit('http://localhost:8081/login')
         cy.login()
         cy.get("[data-cy=profile]").should('include.text', 'Jane Doe')
