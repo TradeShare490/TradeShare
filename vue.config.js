48d533869b63fcc127ac31856af10b5c130140cc
kennguyen0303 <kenn.tnguyen@gmail.com>
2021-11-07 17:16:44 -0500

    add prop in config for deploy

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const GoogleFontsPlugin = require("@beyonk/google-fonts-webpack-plugin")
 // vue.config.js
 module.exports = {
     publicPath: '/',
-    devServer: { port: process.env.PORT || 8081 },
+    devServer: { port: process.env.PORT || 8081, disableHostCheck: true },
 
     configureWebpack: {
         plugins: [
