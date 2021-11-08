2a77e960368fbe7bda42e29396861ac9eeb7ce35
kennguyen0303 <kenn.tnguyen@gmail.com>
2021-11-07 23:25:24 -0500

    final server.js for deploy

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require("express");
-const serveStatic = require("serve-static");
-const history = require("connect-history-api-fallback");
-const path = require("path");
+const express = require('express');
+const history = require('connect-history-api-fallback');
 const app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
-app.use(history());
-
+const path = require("path");
+const staticFileMiddleware = express.static(path.join(__dirname, "dist"));
+app.use(staticFileMiddleware);
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
+app.use(staticFileMiddleware);
 const port = process.env.PORT || 9000;
-app.listen(port);
-console.log("server started " + port);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
