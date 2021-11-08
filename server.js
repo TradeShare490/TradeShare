fc137e6fdc525d2de38b3b6f99509db94950f512
kennguyen0303 <kenn.tnguyen@gmail.com>
2021-11-07 22:53:12 -0500

    debug deploy

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const serveStatic = require("serve-static");
+const history = require("connect-history-api-fallback");
+const path = require("path");
+const app = express();
+app.use(serveStatic(path.join(__dirname, "dist")));
+app.use(history());
+
+const port = process.env.PORT || 9000;
+app.listen(port);
+console.log("server started " + port);
--- src/router/index.js
+++ src/router/index.js
@@ -84,7 +84,7 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  mode: "hash",
+  mode: "history",
   routes,
 });
 router.beforeEach((to, from, next) => {
