96610895fd82afec0f5f5a209d16e24a641c22be
rileykc98 <rileykilmerchoi@hotmail.com>
2021-11-14 21:14:21 -0500

    [TS-127] Fix null pointer

--- cypress/integration/E2E_tests/BrokerageSignUp/brokerageSignUp.spec.js
+++ cypress/integration/E2E_tests/BrokerageSignUp/brokerageSignUp.spec.js
@@ -2,11 +2,11 @@ describe('Brokerage SignUp can', () => {
     before(() => {
         cy.login();
         cy.wait(2000);
-        cy.visit(
-            `${jsonFile.HOMEPAGE}:${process.env.PORT || 8081}${jsonFile.BROKERAGE_SIGNUP}`
-          );
-        cy.wait(2000);
-
+        cy.fixture("url").then((jsonFile) => {
+            cy.visit(
+                `${jsonFile.HOMEPAGE}:${process.env.PORT || 8081}${jsonFile.BROKERAGE_SIGNUP}`
+            );
+        });
     });
     it('Visible', () => {
         cy.get("[data-cy=brokerage-form]").should("be.visible");
