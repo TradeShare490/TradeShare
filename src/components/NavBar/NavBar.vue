3b8123312eae7c56f60e8a0c42f87817e71d9cac
GadoliniumPotassium <gdkh514@gmail.com>
2021-12-23 16:15:10 -0500

    [TS-247]: Redirect on v-slot click has been implemented

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -13,17 +13,31 @@
         TradeShare
       </v-toolbar-title>
       <v-spacer />
+    
       <v-autocomplete
         label="Search"
         append-icon="mdi-magnify"
         class="shrink mr-3"
+        :items="stocks"
+        :loading="IsLoading"
+        :item-text="item => item['symbol'] +': '+ item['name']"
         rounded
+        single-line
         solo
         dense
         light
         hide-details
         hide-no-data
+        clearable
+        click:clear
       >
+        <template v-slot:item="data">
+          <v-list-tile-content
+          v-on:click="redirect(data.item['symbol'])"
+          >
+            <v-list-tile-title ><strong>{{ data.item['symbol'] }}</strong>: {{ data.item['name'] }}</v-list-tile-title>
+          </v-list-tile-content>
+        </template>
       </v-autocomplete>
       <v-btn icon class="mt-1">
         <v-icon>mdi-bell</v-icon>
@@ -38,13 +52,31 @@
 <script>
 export default {
   name: "NavBar",
-  data: () => ({}),
+  data() {
+    return {
+      stocks: [
+        {"symbol": "AAPL", "name": "Apple Inc."}, {"symbol": "TSLA", "name": "Tesla Inc."}, {"symbol": "FSR", "name": "Fisker Inc."}, {"symbol": "SPI", "name": "Spi Energy Co Ltd"}
+      ],
+      isLoading: false
+    };
+  },
+  computed: {
+    items () {
+      return null
+    }
+  },
   methods: {
     logOut() {
       this.$store.dispatch("logout").then(() => {
         this.$router.push({ name: "Login" });
       });
     },
+    redirect(symb){
+      var regex = /^\S*$/;
+      if(symb != "" || regex.test(symb) == false){
+        window.open("https://finance.yahoo.com/quote/"+symb);
+        }
+    }
   },
 };
 </script>
