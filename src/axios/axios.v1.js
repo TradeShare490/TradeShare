f33da8ab51a1bb9a712db1278fc9c0dca325b737
Tung Leu <leudeptrai@gmail.com>
2022-01-08 18:43:13 -0500

    [TS-162] Changed backend host

--- src/axios/axios.v1.js
+++ src/axios/axios.v1.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const HOST = process.env.VUE_APP_BACKEND_HOST || "//localhost:5000/api/v1";
-const HOST = "//localhost:5000/api/v1";
+const HOST = process.env.VUE_APP_BACKEND_HOST || "//localhost:5000/api/v1";
 const axiosInstace = axios.create({
 	baseURL: `${HOST}`,
 });
