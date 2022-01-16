cd5aa2bcaefe91312730b79d246075b308c518ae
Thierry <tianming.chen0424@gmail.com>
2022-01-16 13:03:27 -0500

    [REFACTOR][TS-301] Remove comment useFollowMixin

--- src/hooks/useFollowMixin.js
+++ src/hooks/useFollowMixin.js
@@ -2,25 +2,11 @@ export const useFollowMixin = {
   methods: {
     async follow() {
       console.log("following...");
-      //   try {
-      //     await userFollow({
-      //       target: id,
-      //     });
-      //   } catch {
-      //     console.log("ERROR WHEN FOLLOWING");
-      //   }
       this.following = true;
       console.log("followed");
     },
     async unfollow() {
       console.log("unfollowing...");
-      //   try {
-      //     await userUnfollow({
-      //       target: id,
-      //     });
-      //   } catch {
-      //     console.log("ERROR WHEN FOLLOWING");
-      //   }
       this.following = false;
       console.log("unfollowed");
     },
