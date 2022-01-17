46d324a755141594826176d946a3aea0636eab7e
itsShaVawn <49250036+itsShaVawn@users.noreply.github.com>
2022-01-17 18:23:17 -0500

    [TS-274] TradeZone (new News Feed) implemented (#67)
    * Changed NewsFeed to TradeZone
    
    * Other Dashboard is now a grid
    
    * Removed the search bar from Trending Companies & renamed to Trade Zone
    
    * Implemented the changes to the sidebar
    
    * Removed indentations from script tags
    
    * Changed naming to terms like feed and post for clarity purposes
    
    * Fixed the profile test since the label was removed
    
    * [TS-163] Changed to Router-Link & fixed scroll behavior
    
    * [TS-163] router link on trending companies page
    
    * [TS-274] feed layout in place
    
    * [TS-274] merged with main
    
    * [TS-274] complete
    
    * [TS-274] Fixed responsiveness issues

--- src/components/TradeZone/Feed.vue
+++ src/components/TradeZone/Feed.vue
@@ -1,191 +1,203 @@
 <template>
-    <v-container class="pa-0 ma-0">
-        <div class="d-flex justify-end mb-n3">
-            <v-btn
-                icon
+  <v-container fluid>
+    <v-alert outlined color="grey lighten-2" class="pa-0 ma-0">
+      <div class="d-flex justify-end mb-n3 mr-1">
+        <v-btn icon>
+          <v-icon>mdi-dots-horizontal</v-icon>
+        </v-btn>
+      </div>
+      <v-row no-gutters class="d-flex justify-start ml-5">
+          <v-avatar class="mr-3" size="70">
+            <v-img :src="image" alt="pfp"/>
+          </v-avatar>
+          <div class="d-flex flex-column align-baseline mr-1">
+            <span class="text-no-wrap font-weight-bold black--text text-left">
+            {{ this.name }}
+          </span>
+            <span class="font-weight-light text-subtitle-2 black--text">
+            {{ this.when }}
+          </span>
+          </div>
+          <div id="truncate" class="d-flex flex-column">
+            <span class="font-weight-light text-subtitle-2 black--text">
+              {{ this.username }}
+            </span>
+          </div>
+      </v-row>
+      <v-row no-gutters class="mx-16 px-5 mb-3 d-flex justify-space-between">
+        <v-col>
+          <v-list-item class="px-0">
+            <v-chip
+                label
+                class="white--text px-2 text-h6"
+                color="primary"
             >
-                <v-icon>mdi-dots-vertical</v-icon>
-            </v-btn>
-        </div>
-        <v-row no-gutters justify="space-between" align="center">
-            <v-col xs="12" sm="12" md="6" lg="2" xl="2">
-                <v-list-item class="px-0">
-                    <v-list-item-content>
-                        <v-list-item-avatar class="mr-0">
-                            <v-avatar size="40">
-                                <img :src="image">
-                            </v-avatar>
-                        </v-list-item-avatar>
-                        <v-list-item-title class="text-wrap font-weight-bold">
-                            {{ this.name }}
-                        </v-list-item-title>
-                        <v-list-item class="d-flex justify-center px-0">
-                            <v-chip
-                                label
-                                :class="this.purchased ? 'white--text px-2' : 'white--text px-6'"
-                                :color="this.purchased ? 'green darken-2' : 'red darken-2'"
-                            >
-                                {{ this.purchased ? "PURCHASED" : "SOLD" }}
-                            </v-chip>
-                        </v-list-item>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-col>
-            <v-divider vertical class="my-5 hidden-md-and-down" />
-            <v-col xs="12" sm="12" md="6" lg="3" xl="2">
-                <v-list-item class="px-0">
-                    <v-list-item-content>
-                        <v-col xs="6" sm="6" md="6" lg="12"> 
-                            <v-list-item-title class="text-h6 text-wrap text-left font-weight-medium d-flex justify-start ml-3">
-                                {{ this.company }}
-                            </v-list-item-title>
-                            <v-list-item>
-                                <v-chip
-                                    label
-                                    class="white--text px-2"
-                                    color="primary"
-                                >
-                                    {{ this.tag }}
-                                </v-chip>
-                            </v-list-item>
-                        </v-col>
-                        <v-col xs="6" sm="6" md="6" lg="12">
-                            <v-list-item class="pr-0">
-                                <v-container class="px-0">
-                                    <v-row no-gutters>
-                                        <v-col class="d-flex justify-start mb-1">
-                                            <span class="text-subtitle-2 text-left font-weight-bold">
-                                                Position Size: 
-                                                    <span class="font-weight-medium">
-                                                        {{ this.size }} 
-                                                    </span>
-                                            </span>
-                                        </v-col>
-                                    </v-row>
-                                    <v-row no-gutters>
-                                        <v-col class="d-flex justify-start mb-1 text-no-wrap text-left">
-                                            <span class="text-subtitle-2 font-weight-bold">
-                                                Execution Date: 
-                                                    <span class="font-weight-medium">
-                                                        {{ this.when }}
-                                                    </span>
-                                            </span>
-                                        </v-col>
-                                    </v-row>
-                                    <v-row no-gutters>
-                                        <v-col class="d-flex justify-start">
-                                            <span class="text-subtitle-2 text-left font-weight-bold">
-                                                Verified: 
-                                                    <span class="font-weight-medium">
-                                                        {{ this.verified ? "Yes" : "No" }}
-                                                    </span>
-                                            </span>
-                                        </v-col>
-                                    </v-row>
-                                </v-container>
-                            </v-list-item> 
-                        </v-col>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-col>
-            <v-col xs="12" sm="12" md="6" lg="3" xl="3">
-                <v-list-item class="px-0">
-                    <v-list-item-content class="text-caption text-wrap">
-                            <StockGraph />
-                        <v-list-item>
-                            <v-row>
-                                <v-col>
-                                    <span class="text-h6 font-weight-bold">{{ this.stock1.toFixed(2) }}</span>
-                                </v-col>
-                                <v-col>
-                                    <span class="text-h6 green--text font-weight-bold">{{ this.stock2 }}</span>
-                                </v-col>
-                            </v-row>
-                        </v-list-item> 
-                        <v-list-item>
-                            <v-row>
-                                <v-col>
-                                    <span class="text-subtitle-1 grey--text font-weight-light">{{ this.stock3.toFixed(2) }}</span>
-                                </v-col>
-                                <v-col>
-                                    <span class="text-subtitle-1 grey--text font-weight-light">{{ this.stock4 }}</span>
-                                </v-col>
-                            </v-row>
-                        </v-list-item>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-col>
-            <v-col xs="12" sm="12" md="6" lg="4" xl="5">
-                <v-list-item class="px-0">
-                    <v-list-item-content>
-                        <v-list-item>
-                            <v-alert
-                                class="pa-3"
-                                outlined
-                                color="grey lighten-2"
-                                width=100%
-                                height="100"
-                            >
-                                <div class="black--text">
-                                    <v-row>
-                                <!-- FETCH THESE FROM THE BACK-END -->
-                                        <v-col class="d-flex justify-start">
-                                            Username
-                                        </v-col>
-                                        <v-col class="d-flex justify-start">
-                                            User's Message
-                                        </v-col>
-                                <!-- FETCH THESE FROM THE BACK-END -->
-                                    </v-row>
-                                </div>
-                            </v-alert>
-                        </v-list-item>
-                        <v-list-item>
-                            <v-text-field
-                                label="Comment"
-                                flat
-                                solo
-                                outlined
-                                type="input"
-                            >
-                                <template #append>
-                                    <v-btn
-                                        color="primary"
-                                        type="submit"
-                                        
-                                    >
-                                        Submit
-                                    </v-btn>
-                                </template>
-                            </v-text-field>
-                        </v-list-item>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-col>
-        </v-row>
-        <v-divider></v-divider>
-    </v-container>
+              {{ this.tag }}
+            </v-chip>
+          </v-list-item>
+          <v-list-item-title class="text-h7 text-wrap text-left font-weight-light d-flex justify-start black--text">
+            {{ this.company }}
+          </v-list-item-title>
+        </v-col>
+        <v-col>
+          <v-list-item class="pr-0">
+            <v-container class="px-0">
+              <v-row no-gutters>
+                <v-col class="d-flex justify-start mb-1">
+                    <span class="text-subtitle-2 text-left font-weight-bold">
+                      Position Size: 
+                        <span class="font-weight-medium">
+                            {{ this.size }} 
+                        </span>
+                    </span>
+                </v-col>
+              </v-row>
+              <v-row no-gutters>
+                <v-col class="d-flex justify-start">
+                    <span class="text-subtitle-2 text-left font-weight-bold">
+                      Verified: 
+                        <span class="font-weight-medium">
+                            {{ this.verified ? "Yes" : "No" }}
+                        </span>
+                    </span>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-list-item>
+        </v-col>
+        <v-col>
+          <v-list-item class="px-0">
+            <v-list-item-content>
+              <v-list-item class="d-flex justify-end px-0">
+                <v-chip
+                    label
+                    :class="this.purchased ? 'white--text px-2 font-weight-bold' : 'white--text px-6 font-weight-bold'"
+                    :color="this.purchased ? 'green darken-2' : 'red darken-2'"
+                >
+                  {{ this.purchased ? "PURCHASED" : "SOLD" }}
+                </v-chip>
+              </v-list-item>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
+      <v-row no-gutters class="mx-16">
+        <v-list-item class="px-5 py-0">
+          <v-list-item-content style="background-color: #f2f2f2;">
+            <StockGraph />
+          </v-list-item-content>
+        </v-list-item>
+      </v-row>
+      <v-row no-gutters class="mx-16 px-5 py-2">
+        <v-col>
+          <div class="d-flex flex-column align-xs-center align-sm-start mr-1">
+            <span class="text-h6 font-weight-bold black--text">
+              {{ this.stock1.toFixed(2) }}
+            </span>
+            <span class="text-subtitle-1 grey--text font-weight-light">
+              {{ this.stock3.toFixed(2) }}
+            </span>
+          </div>
+        </v-col>
+        <v-col>
+          <div class="d-flex flex-column align-xs-center align-sm-end mr-1">
+            <span class="text-h6 green--text font-weight-bold">
+              {{ this.stock2 }}
+            </span>
+            <span class="text-subtitle-1 grey--text font-weight-light">
+              {{ this.stock4 }}
+            </span>
+          </div>
+        </v-col>
+      </v-row>
+      <v-row no-gutters class="mb-2">
+        <v-col>
+          <v-btn
+              elevation="0"
+              text
+              x-large
+              :ripple="false"
+              @click="like = !like"
+              :color="like ? 'primary' : ''"
+          >
+            <v-icon> {{ !like ? 'mdi-heart-outline' : 'mdi-heart' }} </v-icon>
+            <span> 12k </span>
+          </v-btn>
+        </v-col>
+        <v-col>
+          <v-btn
+              elevation="0"
+              text
+              x-large
+              :ripple="false"
+          >
+            <v-icon>mdi-comment-processing-outline</v-icon>
+            <span> 8.3k </span>
+          </v-btn>
+        </v-col>
+        <v-col>
+          <v-btn
+              elevation="0"
+              text
+              x-large
+              :ripple="false"
+          >
+            <v-icon>mdi-share-variant-outline</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col>
+          <v-btn
+              elevation="0"
+              text
+              x-large
+              :ripple="false"
+          >
+            <v-icon>mdi-link-variant</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-alert>
+  </v-container>
 </template>
-<script>
-import StockGraph from "../../components/TradeZone/StockGraph.vue"; 
 
+<script>
+import StockGraph from "../../components/TradeZone/StockGraph.vue";
 export default {
   components: { StockGraph },
-    name: "Feed",
-    props: {
-        image: String,
-        name: String,
-        purchased: Boolean,
-        company: String,
-        tag: String,
-        size: Number,
-        when: String,
-        verified: Boolean,
-        stock1: Number,
-        stock2: String,
-        stock3: Number,
-        stock4: String,
-    },
-    data: () => ({}),
+  name: "Feed",
+  props: {
+    image: String,
+    name: String,
+    username: String,
+    when: String,
+    purchased: Boolean,
+    company: String,
+    tag: String,
+    size: Number,
+    verified: Boolean,
+    stock1: Number,
+    stock2: String,
+    stock3: Number,
+    stock4: String,
+  },
+  data() {
+    return {
+      like: false
+    }
+  },
 };
 </script>
+<style scoped>
+.v-btn:before{
+  background-color: transparent;
+}
+.v-btn:hover {
+  color: #3F51B5;
+}
+#truncate {
+  max-width: 14vw;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
