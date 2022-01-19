69380318df53df8133939a1cb8f57f2e979e4bd5
GadoliniumPotassium <gdkh514@gmail.com>
2022-01-18 19:14:06 -0500

    [TS-296]: Fixed a broken test

--- cypress/integration/E2E_tests/NavBarTests/navBar.spec.js
+++ cypress/integration/E2E_tests/NavBarTests/navBar.spec.js
@@ -6,7 +6,7 @@ describe('NavBar can', () => {
     //This test mightg need some work as it's testing something super specific
     it('Redirect you to a Yahoo page',()=>{
         cy.get("[data-cy=autocomplete-search-bar]").should("be.visible");
-        cy.get("[data-cy=autocomplete-search-bar]").type("AAPL").should("have.text","AAPL");
+        cy.get("[data-cy=autocomplete-search-bar]").type("A");
         cy.get("[data-cy=autocomplete-list-content]").should("be.visible");
         cy.get("[data-cy=autocomplete-list-item]").should("be.visible");
         cy.get("[data-cy=autocomplete-list-item]").contains("AAPL").click();
