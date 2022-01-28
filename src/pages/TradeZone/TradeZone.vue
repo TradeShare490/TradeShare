179302537b76185f94c8f7a89d7135071bb62f0c
itsShaVawn <49250036+itsShaVawn@users.noreply.github.com>
2022-01-28 14:04:42 -0500

    [TS-275]: Single Post page functioning & [TS-298] (#76)
    * [TS-275] stash
    
    * [TS-275] single post page functioning
    
    * one code smell gone
    
    * margin fixes
    
    * [TS-275] Fix code duplication
    
    * [TS-275] Add extra post
    
    Co-authored-by: bowe99 <kevinbowe1999@gmail.com>
    Co-authored-by: bowe99 <50396173+bowe99@users.noreply.github.com>

--- src/pages/TradeZone/TradeZone.vue
+++ src/pages/TradeZone/TradeZone.vue
@@ -1,12 +1,12 @@
 <template>
   <v-container fluid>
     <v-row>
-      <v-col xs="12" sm="12" md="12" lg="9" class="d-flex justify-center py-0 mb-2">
-        <v-card min-width="350" width="1000" height="100%" class="mx-10">
+      <v-col xs="12" sm="12" md="12" lg="9" class="d-flex justify-center py-0">
+        <v-card min-width="350" width="800" height="100%" class="mx-10 mb-3">
           <v-row no-gutters>
             <span 
-                class="d-flex justify-start text-h4 font-weight-medium my-3"
-                :class="{'ml-12': $vuetify.breakpoint.xsOnly, 'ml-5': $vuetify.breakpoint.smAndUp}"
+                class="text-h4 font-weight-medium my-3"
+                :class="{'ml-15': $vuetify.breakpoint.xsOnly, 'ml-9': $vuetify.breakpoint.smAndUp}"
             >
               Trade Zone
             </span>
@@ -15,6 +15,7 @@
             <Feed
               v-for="post in allPosts"
               :key="post.id"
+              :id="post.id"
               :image="post.image"
               :name="post.name"
               :username="post.username"
@@ -28,6 +29,7 @@
               :stock2="post.stock2"
               :stock3="post.stock3"
               :stock4="post.stock4"
+              :disabled="false"
             />
           </v-row>
         </v-card>
@@ -45,95 +47,14 @@ import TrendingCompanies from "../../components/TrendingCompanies/TrendingCompan
 
 export default {
   name: "TradeZone",
-  data() {
-    return {
-      allPosts: [
-        {
-          id: 1,
-          image: "https://randomuser.me/api/portraits/men/35.jpg",
-          name: "Ash Britain",
-          username: "@ashcoin63",
-          when: "10min ago",
-          purchased: true,
-          company: "Twitter Inc.",
-          tag: "TWTR",
-          size: 50,
-          verified: false,
-          stock1: 312.92,
-          stock2: "+0.52%",
-          stock3: 311.7,
-          stock4: "Post+0.16%",
-        },
-        {
-          id: 2,
-          image: "https://randomuser.me/api/portraits/men/52.jpg",
-          name: "Tim Robenman",
-          username: "@timthetatman",
-          when: "18h ago",
-          purchased: false,
-          company: "NVIDIA Corp.",
-          tag: "NVDA",
-          size: 150,
-          verified: true,
-          stock1: 222.42,
-          stock2: "+0.41%",
-          stock3: 222.7,
-          stock4: "Post+0.13%",
-        },
-        {
-          id: 3,
-          image: "https://randomuser.me/api/portraits/men/86.jpg",
-          name: "Mac Kafe",
-          username: "@mcafeez",
-          when: "Dec 10",
-          purchased: true,
-          company: "Tesla Inc.",
-          tag: "TSLA",
-          size: 1000,
-          verified: true,
-          stock1: 755.83,
-          stock2: "+1.52%",
-          stock3: 755.1,
-          stock4: "Post+0.91%",
-        },
-        {
-          id: 4,
-          image: "https://randomuser.me/api/portraits/men/35.jpg",
-          name: "Ash Britain",
-          username: "@ashcoin63",
-          when: "Dec 8",
-          purchased: false,
-          company: "Voyager Digital Ltd.",
-          tag: "VYGR",
-          size: 15,
-          verified: false,
-          stock1: 14.98,
-          stock2: "-0.25%",
-          stock3: 15.04,
-          stock4: "Post+0.19%",
-        },
-        {
-          id: 5,
-          image: "https://randomuser.me/api/portraits/men/52.jpg",
-          name: "Tim Robenman",
-          username: "@timthetatman",
-          when: "Nov 29",
-          purchased: false,
-          company: "NVIDIA Corp.",
-          tag: "NVDA",
-          size: 150,
-          verified: true,
-          stock1: 222.42,
-          stock2: "+0.41%",
-          stock3: 222.7,
-          stock4: "Post+0.13%",
-        },
-      ],
-    };
-  },
   components: {
     Feed,
     TrendingCompanies,
   },
-};
+  data() {
+    return {
+      allPosts: this.$store.getters.allPosts
+    }
+  }
+}
 </script>
