78c206bd8b40eaaccba3f7687a95397baf5a7481
Alya Naseer <alyanaseer@gmail.com>
2022-02-08 11:59:58 -0500

    [TS-196] Fixed cypress tests to pass with code changes

--- cypress/integration/E2E_tests/MessagingTests/messaging.spec.js
+++ cypress/integration/E2E_tests/MessagingTests/messaging.spec.js
@@ -1,4 +1,4 @@
-describe("On the messages page, can", () => {
+describe("On the messaging page, can", () => {
   before(() => {
     cy.login();
     cy.wait(2000);
@@ -8,12 +8,10 @@ describe("On the messages page, can", () => {
       );
     });
   });
-  it("display no messages at first", () => {
-    cy.get("[data-cy=no-messages-window]").should("be.visible");
-  });
   it("select a conversation", ()=> {
-    cy.get("[data-cy=chat]").click()
-    cy.get("[data-cu=messages-window]").should("be.visible");
+    cy.get("[data-cy=no-messages-window]").should("be.visible");
+    cy.get("[data-cy=chat]").first().click()
+    cy.get("[data-cy=messages-window]").should("be.visible");
   })
   it("search for people", () => {
     cy.get("[data-cy=search]").click().type("Jane Doe");
@@ -22,9 +20,11 @@ describe("On the messages page, can", () => {
     cy.get("[data-cy=new-message]").click();
   });
   it("can press the emoji button",()=>{
+    cy.get("[data-cy=chat]").first().click()
     cy.get("[data-cy=image-button]").should("be.visible").click();
   })
   it("send a message", () => {
+    cy.get("[data-cy=chat]").first().click()
     cy.get("[data-cy=send-message]").click().type("Hello");
     cy.get("[data-cy=send-button]").click();
     cy.get("[data-cy=messages-window]").should("include.text", "Hello");
