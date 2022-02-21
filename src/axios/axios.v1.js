22e23c389c7df8e4125b9ce836a0ad81a9eca270
Ken Nguyen <kenn.tnguyen@gmail.com>
2022-02-21 16:43:57 -0500

    [TS-417] Choose server by mode

--- src/axios/axios.v1.js
+++ src/axios/axios.v1.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
-const HOST = process.env.VUE_APP_BACKEND_HOST || '//localhost:5000/api/v1'
+const HOST_BY_MODE = {
+	'development': process.env.VUE_APP_BACKEND_HOST,
+	'production': process.env.VUE_APP_BACKEND_DEV 
+}
+
+const HOST = HOST_BY_MODE[process.env.NODE_ENV] || "//localhost:5000/api/v1";
 const axiosInstace = axios.create({
   baseURL: `${HOST}`
 })
