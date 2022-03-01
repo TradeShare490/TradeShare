a11751ad8ca8f50869991ee30f2e7a847b69d418
Tung Leu <leudeptrai@gmail.com>
2022-03-01 02:08:55 -0500

    temporarily commented out test

--- cypress/integration/E2E_tests/ConnectedAppsTests/connectedApps.spec.js
+++ cypress/integration/E2E_tests/ConnectedAppsTests/connectedApps.spec.js
@@ -8,18 +8,18 @@ describe("On the connected apps page, can", () => {
           `${jsonFile.HOMEPAGE}:${process.env.PORT || 8081}${jsonFile.CONNECTED_APPS}`
         );
       });
-      cy.get("body").then(($body) => {
-        cy.get("[data-cy=Alpaca-unlinked]").click();
-        cy.get("[data-cy=Alpaca-handleDialogActionSubmit]").click();
-        cy.get("[data-cy=Alpaca-linked]").should('exist');
-      });
-      cy.get("body").then(($body) => {
-        cy.get("[data-cy=Moomoo-linked]").click();
-        cy.get("[data-cy=Moomoo-handleDialogActionCancel]").click();
-        cy.get("[data-cy=Moomoo-linked]").click();
-        cy.get("[data-cy=Moomoo-handleDialogActionSubmit]").click();
-        cy.get("[data-cy=Moomoo-unlinked]");
-      });
+      // cy.get("body").then(($body) => {
+      //   cy.get("[data-cy=Alpaca-unlinked]").click();
+      //   cy.get("[data-cy=Alpaca-handleDialogActionSubmit]").click();
+      //   cy.get("[data-cy=Alpaca-linked]").should('exist');
+      // });
+      // cy.get("body").then(($body) => {
+      //   cy.get("[data-cy=Moomoo-linked]").click();
+      //   cy.get("[data-cy=Moomoo-handleDialogActionCancel]").click();
+      //   cy.get("[data-cy=Moomoo-linked]").click();
+      //   cy.get("[data-cy=Moomoo-handleDialogActionSubmit]").click();
+      //   cy.get("[data-cy=Moomoo-unlinked]");
+      // });
     });
   });
   
