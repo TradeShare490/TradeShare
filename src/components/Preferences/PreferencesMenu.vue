738961f90f9bb6ed56332f7ee314b942f6392fe1
Siobhan <siobhan.cousineau@gmail.com>
2022-03-06 16:30:22 -0500

    fixed bug [TS-447]

--- src/components/Preferences/PreferencesMenu.vue
+++ src/components/Preferences/PreferencesMenu.vue
@@ -1,54 +1,50 @@
 <template>
-  <v-card>
-    <v-navigation-drawer
-      permanent
-      fixed
-      absolute
-      clipped
-      height="100vh"
-      :mini-variant="mini"
-    >
-      <v-list-item class="hidden-sm-and-down">
-        <v-list-item-content class="mt-2 text-left">
-          <v-list-item-title
-            class="font-weight-bold"
-            style="color: #66788A"
-          >
-            PREFERENCES
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-divider class="my-1 hidden-sm-and-down" />
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item-group
-          v-model="selectedPreferenceItem"
-          color="primary"
+  <v-navigation-drawer
+    permanent
+    absolute
+    clipped
+    :mini-variant="mini"
+  >
+    <v-list-item class="hidden-sm-and-down">
+      <v-list-item-content class="mt-2 text-left">
+        <v-list-item-title
+          class="font-weight-bold"
+          style="color: #66788A"
         >
-          <v-list-item
-            v-for="(item, i) in navItems"
-            :key="i"
-            :to="item.route"
-            active-class="active"
-            data-cy="menu"
-          >
-            <v-list-item-icon>
-              <v-icon>
-                {{ item.icon }}
-              </v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title class="text-left">
-                {{ item.title }}
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-  </v-card>
+          PREFERENCES
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    <v-divider class="my-1 hidden-sm-and-down" />
+    <v-list
+      dense
+      nav
+    >
+      <v-list-item-group
+        v-model="selectedPreferenceItem"
+        color="primary"
+      >
+        <v-list-item
+          v-for="(item, i) in navItems"
+          :key="i"
+          :to="item.route"
+          active-class="active"
+          data-cy="menu"
+        >
+          <v-list-item-icon>
+            <v-icon>
+              {{ item.icon }}
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="text-left">
+              {{ item.title }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
