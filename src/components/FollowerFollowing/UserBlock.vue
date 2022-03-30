fdc7157bcdbffde143633eee42babc9f7ecc035d
Thierry <tianming.chen0424@gmail.com>
2022-03-30 08:48:28 -0400

    Update UserBlock.vue

--- src/components/FollowerFollowing/UserBlock.vue
+++ src/components/FollowerFollowing/UserBlock.vue
@@ -45,7 +45,7 @@
               <v-list-item-title
                 class="name-text text-sm-body-1 text-caption text-wrap font-weight-bold"
               >
-                {{ name }}  [{{ isPrivate }}] {{ id }}
+                {{ name }}
               </v-list-item-title>
               <v-list-item-title class="text-sm-body-3 text-caption text-wrap">
                 @{{ username }}
