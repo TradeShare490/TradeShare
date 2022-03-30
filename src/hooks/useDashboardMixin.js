9a1804d18a931b7583b7380fd716447958d04367
Ming424 <tianming.chen0424@gmail.com>
2022-03-30 03:44:11 -0400

    fixed an undefined issue

--- src/hooks/useDashboardMixin.js
+++ src/hooks/useDashboardMixin.js
@@ -15,25 +15,27 @@ export const useDashboardMixin = {
   },
   methods: {
     handleHoldingPieChartData () {
-      const sumCash = Number(this.account.cash)
-      let sumEquity = 0
-      let sumOption = 0
-      let numEquity = 0
-      let numOption = 0
-      this.stocks.forEach(stock => {
-        if (stock.asset_class.toLowerCase().includes('equity')) {
-          numEquity++
-          sumEquity += Number(stock.market_value)
-        } else if (stock.asset_class.toLowerCase().includes('option')) {
-          numOption++
-          sumOption += Number(stock.market_value)
-        }
-      })
-      this.holdingPieChartData.sumCash = sumCash
-      this.holdingPieChartData.numEquity = numEquity
-      this.holdingPieChartData.sumEquity = sumEquity
-      this.holdingPieChartData.numOption = numOption
-      this.holdingPieChartData.sumOption = sumOption
+      if (this.stocks && this.stocks.length !== 0) {
+        const sumCash = Number(this.account.cash)
+        let sumEquity = 0
+        let sumOption = 0
+        let numEquity = 0
+        let numOption = 0
+        this.stocks.forEach(stock => {
+          if (stock.asset_class.toLowerCase().includes('equity')) {
+            numEquity++
+            sumEquity += Number(stock.market_value)
+          } else if (stock.asset_class.toLowerCase().includes('option')) {
+            numOption++
+            sumOption += Number(stock.market_value)
+          }
+        })
+        this.holdingPieChartData.sumCash = sumCash
+        this.holdingPieChartData.numEquity = numEquity
+        this.holdingPieChartData.sumEquity = sumEquity
+        this.holdingPieChartData.numOption = numOption
+        this.holdingPieChartData.sumOption = sumOption
+      }
     }
   }
 }
