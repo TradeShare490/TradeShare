0d70bd23d8247d59adca1f70d94a68de68634788
GeoDan K <57120259+GadoliniumPotassium@users.noreply.github.com>
2022-03-30 08:36:03 -0400

    [TS-408] Forgot Password! (#123)
    * [TS-408]: Forgot Password Restarted
    
    * [TS-408]: did some linting and fixed imports
    
    * TS-408
    :Progress made
    
    * [TS-408]: added code to pull proper UID
    
    * [TS-408]: Almost done with forgot password
    
    * Forgot Password Additions
    
    * refactored axiosInstance
    
    * refactored axiosInstance and tried to updatea the patch
    
    * I broke the forgot password page but I'm closer to getting the proper parsing
    
    * Attempted to finish this did not test
    
    * Password still won't show but the code logic is sound
    
    * [TS-408]: Forget password implemented
    
    * [TS-408]: Prenultimate commit for the forgot password saga. Fixed a page and added a router link!
    
    * [TS-408]: fixed code smell
    
    * [TS-408]: Made forgot password pages public
    
    * fixed typos and errors

---
+++ src/pages/ForgotPassword/ForgotPasswordEmail.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container mt-0 mx-auto">
+    <div class="title mt-6 mb-11 px-7">
+      Forgot Password (Enter Email)
+    </div>
+    <div>
+      <v-form
+        ref="emailForm"
+        v-model="valid"
+      >
+        <p>
+          Enter your email and if your email exists then we will send you a password replacement form.
+        </p>
+        <br>
+        <v-label for="enter-email">
+          Enter Email:
+        </v-label>
+        <v-text-field
+          v-model="email"
+          class="my-5"
+          data-cy="email"
+          label="Email"
+          color="primary"
+          :rules="[rules.required, rulesEmail.format]"
+          @keyup.enter="submit"
+        />
+        <br>
+        <v-btn
+          width="250"
+          height="45"
+          color="primary"
+          class="my-3 text-button"
+          data-cy="login-button"
+          @click="submit"
+        >
+          Submit
+        </v-btn>
+      </v-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import axiosInstance from '../../axios/axios.v1'
+export default {
+  name: 'EmailSender',
+  data: () => ({
+    valid: '',
+    email: '',
+    rules: {
+      required: v => !!v || 'Required'
+    },
+    rulesEmail: {
+      format: v =>
+        /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()\\[\]\\.,;:\s@']+)*)|('.+'))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          v
+        ) || 'E-mail must be valid'
+    }
+  }),
+  methods: {
+    async submit () {
+      if (this.$refs.emailForm.validate()) {
+        try {
+          const UserInfo = await axiosInstance.get(`/userInfo?searchQuery=${this.email}&limit=1`)
+          const MailOption = {
+            to: this.email,
+            subject: 'TradeShare: Forgot Password',
+            text: `Change your password in the following link http://localhost:8081/forgotpassword/userId=${UserInfo.data.data[0].userId}`
+          }
+          console.log(MailOption.text)
+          return await axiosInstance.post('/mailer', MailOption)
+        } catch (e) {
+          this.error = e
+        }
+      }
+    }
+  }
+}
+</script>
