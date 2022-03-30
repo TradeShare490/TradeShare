4a2c604a33fb94bca8723a7bd41b0fdcced24ed4
Tung Leu <leudeptrai@gmail.com>
2022-03-30 05:53:52 -0400

    fixed tests

--- cypress/integration/E2E_tests/ComparisonToolTests/comparisonTool.spec.js
+++ cypress/integration/E2E_tests/ComparisonToolTests/comparisonTool.spec.js
@@ -3,6 +3,7 @@ describe("On comparison page, can", () => {
   it("open the modal", () => {
     cy.login();
     cy.wait(5000);
+    cy.get("[data-cy=profile]").should("include.text", "Jane Doe");
     cy.get("[data-cy=modal-activate]").click();
     cy.get("[data-cy=modal]").should("be.visible");
   });
